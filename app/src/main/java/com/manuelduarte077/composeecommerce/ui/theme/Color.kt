cbdce7fcb8eddae77dd66f902292b78ca17045ce
Manuel Duarte <carlosd99m@gmail.com>
2021-12-03 22:48:47 -0600

    feat: adding theme colors

--- app/src/main/java/com/manuelduarte077/composeecommerce/ui/theme/Color.kt
+++ app/src/main/java/com/manuelduarte077/composeecommerce/ui/theme/Color.kt
@@ -6,3 +6,17 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val primaryColor = HexToColor.getColor("#ed4960")
+val white = Color.White
+
+val black =  HexToColor.getColor("#222a32")
+val backgroundDark =  HexToColor.getColor("#19202c")
+val surfaceDark =  HexToColor.getColor("#1c2439")
+
+
+object HexToColor{
+    fun getColor (color: String): Color {
+        return Color(android.graphics.Color.parseColor(color))
+    }
+}
--- app/src/main/java/com/manuelduarte077/composeecommerce/ui/theme/Theme.kt
+++ app/src/main/java/com/manuelduarte077/composeecommerce/ui/theme/Theme.kt
@@ -7,15 +7,21 @@ import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
-    primary = Purple200,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = primaryColor,
+    onPrimary = white,
+    background = backgroundDark,
+    onBackground = white,
+    surface = surfaceDark,
+    onSurface = white
 )
 
 private val LightColorPalette = lightColors(
-    primary = Purple500,
-    primaryVariant = Purple700,
-    secondary = Teal200
+    primary = primaryColor,
+    onPrimary = white,
+    background = white,
+    onBackground = black,
+    surface = white,
+    onSurface = black
 
     /* Other default colors to override
     background = Color.White,
