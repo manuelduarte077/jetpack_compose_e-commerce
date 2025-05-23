b06bdc096e206f9318d8233fb330b7c7dc70134a
manuelduarte077 <carlosd99m@gmail.com>
2025-05-22 23:57:16 -0600

    add resources

--- ComposeE-commerce/composeApp/src/commonMain/kotlin/dev/donmanuel/ecommerce/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package dev.donmanuel.ecommerce
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- ComposeE-commerce/composeApp/src/commonMain/kotlin/dev/donmanuel/ecommerce/Platform.kt
+++
@@ -1,7 +0,0 @@
-package dev.donmanuel.ecommerce
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
